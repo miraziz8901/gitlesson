fd05f0dfd1fbebfe9eeee59d52a3379b74889a3f
miraziz8901 <112271350+miraziz8901@users.noreply.github.com>
2022-10-28 22:17:06 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,136 @@
+"use strict";
+
+// math operators  + , - , * , / , % , ** , ++ incr , --decr    
+
+const a = 4;
+const b = 6;
+
+
+console.log(a*b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var fruits='apple';
+// fruits='limon'
+// var fruits="apelsin";
+
+// let fruits='apple';
+// fruits='limon'
+// fruits='peach'
+// fruits='ananas'
+// let fruits='banana'; no correcrt
+
+
+// const fruits='apple';
+// fruits='aplesin'
+// const fruits='limon';
+
+// =============== 2 ===============////
+
+
+// math operators  + , - , * , / , % , ** , 
+// ++ INCR , -- DECR
+
+// > , < , >= , <= , == v , === v , t, ! 
+// is not  
+
+// let a=10;
+// let b=8;
+
+// a=a+b;
+// a=a*b;
+// a=a/b;
+// a=a%b;
+// a=a**b;
+
+// a+=b;
+// a-=b;
+// a*=b;
+// a**=b;
+// a%=b;
+// a/=b;
+
+
+
+// console.log(a);
+
+// =========== 3 ===========
+
+
+// let rang='tree';
+
+// if(rang==='blue'){
+
+//     console.log('no reeeed');
+
+// }else if (rang==='red'){
+
+//     console.log('red');
+
+// }else {
+//     console.log('reeeed');
+// }
+
+   
+
+
+
+// let light='transparent';
+
+// if(light==='red'){
+
+//    console.log('GO');
+
+// }else if(light==='yellow'){
+
+//    console.log('warning');
+
+// }else{
+
+//    console.log('STOP or WARNING')
+// }
+
+
+
+// //////////////////   homeWork  /////////////
+
+
+
+
+
+// let b=10;
+//     b=12;
+
+// console.log(b+b);
+
+
+
+
+
